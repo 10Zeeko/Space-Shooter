45c5e4be6101d21f4fcc6cb82385a54dd589d615
Zeeko <10polgb@gmail.com>
2023-10-02 20:25:48 +0200

    First Commit with background

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+
+FPS=60
+WINDOW_SIZE_X=800
+WINDOW_SIZE_Y=800
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode([WINDOW_SIZE_X, WINDOW_SIZE_Y])
+    game_icon = pygame.image.load('assets/icon.png')
+    background_image = pygame.image.load('assets/background.png')
+    background_size = background_image.get_rect()
+
+    pygame.display.set_caption('Space Shooter')
+    pygame.display.set_icon(game_icon)
+
+    clock = pygame.time.Clock()
+    going = True
+    while (going):
+        delta = clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                going = False
+        screen.blit(background_image,background_size)
+        pygame.display.flip()
+    pygame.quit()
+
+main()
